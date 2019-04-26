4dc86d48575abed274e5ce8324d882059754a5e4
watsa9604 <watsa9604@HRH-IC0037410.wrdsb.ca>
2019-04-26 13:11:26 -0400

    Customer andy v.0.0.1

--- NetBeansProjects/Banksy/src/banksy/Bank.java
+++ NetBeansProjects/Banksy/src/banksy/Bank.java
@@ -13,54 +13,56 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Bank {
-    
-    private List<Customer> Customer= new ArrayList<>();
-    
-    public Bank(){
-        
+
+    private List<Customer> Customer = new ArrayList<>();
+
+    public Bank() {
+
+    }
+
+    public Bank(String Customer) {
+
     }
     
-    public Bank(String Customer){
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public void checkBal(int n, String filename){
-        try{
-            
-            for(Customer m: this.Customer){
-            
-            System.out.println(" " + m.getBal() );
-            
+
+    public void checkBal(int n, String filename) {
+        try {
+
+            for (Customer m : this.Customer) {
+
+                System.out.println(" " + m.getBal());
+
             }
-        }catch(Exception e){
-            System.err.println("Oopsiewoopsie, sumtin went wong: " +e);
-        }
-    }
-    
-    
-    public void findCustomer(int n, String filename){
-        try{
-            
-            Scanner output = new Scanner(new File(filename));
-            
-            for(Customer m: this.Customer){
-                
-                    
-                
-            }
-            
-        }catch(Exception e){
+        } catch (Exception e) {
             System.err.println("Oopsiewoopsie, sumtin went wong: " + e);
         }
     }
-    
-    
+
+    public void findCustomer(String filename) {
+        try {
+
+            Scanner input = new Scanner(new File(filename));
+            while (input.hasNext()) {
+                for (Customer m : this.Customer) {
+                    //adds everything that will be listed
+                    int accNum = input.nextInt();
+                    input.nextLine();
+                    String name = input.nextLine();
+                    String address = input.nextLine();
+                    int pin = input.nextInt();
+                    input.nextLine();
+                    int bal = input.nextInt();
+                    input.nextLine();
+                    boolean type = input.nextBoolean();
+                    int lim = input.nextInt();
+                    input.nextLine();
+                    //if over credit limit track them down
+
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Oopsiewoopsie, sumtin went wong: " + e);
+        }
+    }
+
 }
