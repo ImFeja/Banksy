99bce8ddfa44d5556f752c13f1a0918d10a5a71d
OGfej <OGfej@Fejas-Zenbook>
2019-04-26 12:58:08 -0400

    First Commit

--- NetBeansProjects/Banksy/src/banksy/Customer.java
+++ NetBeansProjects/Banksy/src/banksy/Customer.java
@@ -14,10 +14,10 @@ public class Customer {
     private String name;
     private String address;
     private double balance;
-    private int credit;
+    private double credit;
     
     
-    public Customer(String name, String address, double balance, int credit){
+    public Customer(String name, String address, double balance, double credit){
         this.name = name;
         this.address = address;
         this.balance = balance;
@@ -35,6 +35,19 @@ public class Customer {
     
     public double getBal(){
        return this.balance;
+    }
+    
+    //Use when making purchase, are you over your credit limit? If it will go over it will return true, so use if = true then block it.
+    public boolean overCredit(double b, double n){
+        b = this.balance;
+        
+        if(b - n < this.credit){
+            return true;
+        }else{
+            return false;
+        }
+        
+        
     }
            
     
