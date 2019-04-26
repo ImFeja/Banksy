e4a9038f718327ed66b6c385b23f34c0e953e951
OGfej <OGfej@Fejas-Zenbook>
2019-04-26 12:57:06 -0400

    First Commit

--- NetBeansProjects/Banksy/src/banksy/Bank.java
+++ NetBeansProjects/Banksy/src/banksy/Bank.java
@@ -5,10 +5,62 @@
  */
 package banksy;
 
-/**
- *
- * @author OGfej
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Bank {
+    
+    private List<Customer> Customer= new ArrayList<>();
+    
+    public Bank(){
+        
+    }
+    
+    public Bank(String Customer){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void checkBal(int n, String filename){
+        try{
+            
+            for(Customer m: this.Customer){
+            
+            System.out.println(" " + m.getBal() );
+            
+            }
+        }catch(Exception e){
+            System.err.println("Oopsiewoopsie, sumtin went wong: " +e);
+        }
+    }
+    
+    
+    public void findCustomer(int n, String filename){
+        try{
+            
+            Scanner output = new Scanner(new File(filename));
+            
+            for(Customer m: this.Customer){
+                
+                    
+                
+            }
+            
+        }catch(Exception e){
+            System.err.println("Oopsiewoopsie, sumtin went wong: " + e);
+        }
+    }
+    
     
 }
